689bc38e18916ef6dd97c495bab3a1010f367597
Myles Scolnick <myles@marimo.io>
2024-01-16 21:46:36 -0500

    imrpove hints

--- frontend/src/core/codemirror/completion/hints.ts
+++ frontend/src/core/codemirror/completion/hints.ts
@@ -1,49 +1,53 @@
 /* Copyright 2023 Marimo. All rights reserved. */
 import { HTMLCellId } from "@/core/cells/ids";
-import { hoverTooltip } from "@codemirror/view";
+import { EditorView, hoverTooltip } from "@codemirror/view";
 import { AUTOCOMPLETER, Autocompleter } from "./Autocompleter";
 import { Logger } from "@/utils/Logger";
-import { Text } from "@codemirror/state";
+import { EditorState, Text } from "@codemirror/state";
+import { debounce } from "lodash-es";
+import { documentationAtom } from "@/core/documentation/state";
+import { store } from "@/core/state/jotai";
 
 export function hintTooltip() {
   return [
-    hoverTooltip(
-      async (view, pos) => {
-        const cellContainer = HTMLCellId.findElement(view.dom);
-        if (!cellContainer) {
-          Logger.error("Failed to find active cell.");
-          return null;
-        }
-
-        const cellId = HTMLCellId.parse(cellContainer.id);
-
-        const { startToken, endToken } = getPositionAtWordBounds(
-          view.state.doc,
-          pos
-        );
-
-        const result = await AUTOCOMPLETER.request({
-          document: view.state.doc.slice(0, endToken).toString(), // convert Text to string
-          cellId: cellId,
-        });
-        if (!result) {
-          return null;
-        }
-
-        const fullWord = view.state.doc.slice(startToken, endToken).toString();
-        const tooltip = Autocompleter.asHoverTooltip({
-          position: endToken,
-          message: result,
-          exactName: fullWord,
-          excludeTypes: ["tooltip"],
-        });
-        return tooltip ?? null;
-      },
-      {
-        hideOnChange: true,
-      }
-    ),
+    hoverTooltip((view, pos) => requestDocumentation(view, pos, ["tooltip"]), {
+      hideOnChange: true,
+    }),
+    cursorPositionDocumentation,
   ];
+}
+
+async function requestDocumentation(
+  view: EditorView,
+  pos: number,
+  excludeTypes?: string[]
+) {
+  const cellContainer = HTMLCellId.findElement(view.dom);
+  if (!cellContainer) {
+    Logger.error("Failed to find active cell.");
+    return null;
+  }
+
+  const cellId = HTMLCellId.parse(cellContainer.id);
+
+  const { startToken, endToken } = getPositionAtWordBounds(view.state.doc, pos);
+
+  const result = await AUTOCOMPLETER.request({
+    document: view.state.doc.slice(0, endToken).toString(), // convert Text to string
+    cellId: cellId,
+  });
+  if (!result) {
+    return null;
+  }
+
+  const fullWord = view.state.doc.slice(startToken, endToken).toString();
+  const tooltip = Autocompleter.asHoverTooltip({
+    position: endToken,
+    message: result,
+    exactName: fullWord,
+    excludeTypes: excludeTypes,
+  });
+  return tooltip ?? null;
 }
 
 export function getPositionAtWordBounds(doc: Text, pos: number) {
@@ -72,3 +76,29 @@ export function getPositionAtWordBounds(doc: Text, pos: number) {
 
   return { startToken, endToken };
 }
+
+// Checks if the cursor is in a text element
+function isCursorInText(state: EditorState) {
+  const { head } = state.selection.main;
+  const text = state.doc.sliceString(head - 1, head);
+  return /\w/.test(text);
+}
+
+// Debounce the request to avoid spamming the server
+const debouncedAutocomplete = debounce(
+  async (view: EditorView, position: number) => {
+    const tooltip = await requestDocumentation(view, position);
+    store.set(documentationAtom, {
+      documentation: tooltip?.html ?? null,
+    });
+  },
+  200
+);
+
+// Listen to cursor movement and send documentation requests
+const cursorPositionDocumentation = EditorView.updateListener.of((update) => {
+  if (update.selectionSet && isCursorInText(update.state)) {
+    const cursorPos = update.state.selection.main.head;
+    debouncedAutocomplete(update.view, cursorPos);
+  }
+});
